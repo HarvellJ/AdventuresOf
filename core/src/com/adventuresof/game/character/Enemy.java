9ecd371d0264b82faec6b7401604448b8f3faebe
Jake Woakes <Jake@localhost>
2018-11-12 08:47:15 +0000

    WIP

--- core/src/com/adventuresof/game/character/Enemy.java
+++ core/src/com/adventuresof/game/character/Enemy.java
@@ -1,13 +1,24 @@
 package com.adventuresof.game.character;
 
 import com.adventuresof.helpers.AnimationFactory;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 public class Enemy extends NPC {
 
+	private int width, height;
+	private float speed, x, y, health, startHealth;
+	private Texture texture, healthBackground, healthForeground, healthBorder;
+	
 	public Enemy(TiledMapTileLayer accessibleTiles, String textureSheet, float startX, float startY) {
 		super(accessibleTiles, textureSheet, 8, 5, startX, startY, false, true, 50, 50);
 		this.setCharacterDirection(Direction.left);
+		this.health = health;
+		this.startHealth = health;
+		this.healthBackground = QuickLoad("healthBackground");
+		this.healthForeground = QuickLoad("healthForeground");
+		this.healthBorder = QuickLoad("healthBorder");
+		
 	}
 
 	protected void createAnimations() {
@@ -30,6 +41,11 @@ public class Enemy extends NPC {
 		this.deathUpAnimation = AnimationFactory.createAnimation(this.animationSheetName, frameCols, frameRows, 3, 30, 1f);
 		this.deathRightAnimation = AnimationFactory.createAnimation(this.animationSheetName, frameCols, frameRows, 3, 33, 1f);
 		this.deathLeftAnimation = AnimationFactory.createAnimation(this.animationSheetName, frameCols, frameRows, 3, 36, 1f);
-	}    
+	}   
+	
+	public void draw() {
+		DrawQuadTex(healthBackground x, y, width, height);
+	}
+	}
 	
 }
