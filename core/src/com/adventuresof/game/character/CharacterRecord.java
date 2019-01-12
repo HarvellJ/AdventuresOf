593fd80086d1370541812f8813dd81097b61d2cf
Jake Woakes <>
2019-01-12 17:13:36 +0000

    Final characters

--- core/src/com/adventuresof/game/character/CharacterRecord.java
+++ core/src/com/adventuresof/game/character/CharacterRecord.java
@@ -8,12 +8,15 @@ public class CharacterRecord {
 		name = _name;
 	}
 	
-    public static String CHAR_NAME_ELF = "bloodelf";
-    public static String CHAR_NAME_MAGE = "blueWizard";
+    public static String CHAR_NAME_KNIGHT = "knightSprite";
+    public static String CHAR_NAME_MAGE = "mageSprite";
+    public static String CHAR_NAME_ARCHER = "archerSprite";
+
     
     public static CharacterRecord CHARACTERS[] =
     {
-        new CharacterRecord(CHAR_NAME_ELF),
-        new CharacterRecord(CHAR_NAME_MAGE)
+        new CharacterRecord(CHAR_NAME_KNIGHT),
+        new CharacterRecord(CHAR_NAME_MAGE),
+        new CharacterRecord(CHAR_NAME_ARCHER)
     };
 }
--- core/src/com/adventuresof/screens/Resources.java
+++ core/src/com/adventuresof/screens/Resources.java
@@ -13,16 +13,21 @@ public class Resources {
 	public BitmapFont gamefont;
 	public Sprite player;
 	public Sprite player2;
+	public Sprite player3;
 	public HashMap<String, Sprite> playerSprites;
 
 	public Resources() {
 		gamefont = new BitmapFont(Gdx.files.internal("gamefont.fnt"), Gdx.files.internal("gamefont.png"), false);
-		player = new Sprite(new Texture("characterSelection/" + CharacterRecord.CHAR_NAME_ELF + ".png"));
+		player = new Sprite(new Texture("characterSelection/" + CharacterRecord.CHAR_NAME_KNIGHT + ".png"));
 		player2 = new Sprite(new Texture("characterSelection/" + CharacterRecord.CHAR_NAME_MAGE + ".png"));
+		player3 = new Sprite(new Texture("characterSelection/" + CharacterRecord.CHAR_NAME_ARCHER + ".png"));
+
 
 		playerSprites = new HashMap<String, Sprite>();
-		playerSprites.put(CharacterRecord.CHAR_NAME_ELF, player);
+		playerSprites.put(CharacterRecord.CHAR_NAME_KNIGHT, player);
 		playerSprites.put(CharacterRecord.CHAR_NAME_MAGE, player2);
+		playerSprites.put(CharacterRecord.CHAR_NAME_ARCHER, player3);
+
 
 	}
 
